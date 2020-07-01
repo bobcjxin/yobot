4a00078d42bc365b2e552b214b2eae8ba6fc8e6e
ChenJun <bob330128901@gmail.com>
2020-07-01 16:59:32 +0800

    在公会战页面增加一个跳转到公会战排名查询网站的按钮

--- src/client/public/static/clan/panel.js
+++ src/client/public/static/clan/panel.js
@@ -268,6 +268,9 @@ var vm = new Vue({
                 case '5':
                     window.location = `./${this.self_id}/`;
                     break;
+                case '6':
+                    window.location = 'https://kengxxiao.github.io/Kyouka/';
+                    break;
             }
         },
     },
